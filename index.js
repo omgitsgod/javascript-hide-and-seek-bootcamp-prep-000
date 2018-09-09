13391ed22c51c9396eed8f8463ccae990d3eee0c
Ryan Travis <rpenchenski@gmail.com>
2018-09-09 00:55:16 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ function increaseRankBy(n) {
   const ranky = document.querySelectorAll('#ranked-list')
   
   for (var i = 0; i < ranky.length; i++ ) {
-    ranky[i].innerHTML = parseInt(rank[i].innerHTML + n)
+    ranky[i].innerHTML = parseInt(ranky[i].innerHTML + n)
   }
 }
 
