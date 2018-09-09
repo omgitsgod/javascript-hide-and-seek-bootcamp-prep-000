f826aeceacb415946a50f72a0e398491e88b8b4d
Ryan Travis <rpenchenski@gmail.com>
2018-09-09 00:44:53 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
+function getFirstSelector(selector) {
+  return document.querySelector(selector)
+}
 
+function nestedTarget() {
+  return document.querySelector('#nested .target');
+}
+
+
+function increaseRankBy(n) {
+  
+}
+
+function deepestChild() {
+  
+}
