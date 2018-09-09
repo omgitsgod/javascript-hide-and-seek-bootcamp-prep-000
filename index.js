c88d37e2ab2abd486cb97fc136ff25871658f97c
Ryan Travis <rpenchenski@gmail.com>
2018-09-09 01:04:42 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -15,3 +15,11 @@ function increaseRankBy(n) {
   }
 }
 
+function deepestChild() {
+  var lis=document.getElementById("grand-node").querySelectorAll("div");
+  var test;
+  for(let i=0;i<lis.length-1;i++) {
+    test=lis[i].querySelector("div");
+  }
+return test;
+}
