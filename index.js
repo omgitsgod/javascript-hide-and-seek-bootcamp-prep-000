bad1ce0d94aec13a2bf3ae78c798316843cb2b9b
Ryan Travis <rpenchenski@gmail.com>
2018-09-09 01:08:09 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
+function getFirstSelector(selector) {
+  return document.querySelector(selector)
+}
 
+function nestedTarget() {
+  return document.querySelector('#nested .target');
+}
+
+
+function increaseRankBy(n) {
+  const ranky = document.querySelectorAll('ul.ranked-list li')
+  
+  for (var i = 0; i < ranky.length; i++ ) {
+    ranky[i].innerHTML = parseInt(ranky[i].innerHTML) + n;
+  }
+}
+
+function deepestChild() {
+  var grr = document.getElementById("grand-node").querySelectorAll("div");
+  var perhaps;
+  for (let i=0;i<grr.length-1;i++) {
+    perhaps = grr[i].querySelector("div");
+  }
+return perhaps;
+}
